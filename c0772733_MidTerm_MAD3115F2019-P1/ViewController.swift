d002225478387e481655fcaef784224c515b9f26
kamaljit1504 <kaurkamaljit963@gmail.com>
2019-10-30 21:24:00 -0400

    test

--- c0772733_MidTerm_MAD3115F2019-P1/ViewController.swift
+++ c0772733_MidTerm_MAD3115F2019-P1/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnLS: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
