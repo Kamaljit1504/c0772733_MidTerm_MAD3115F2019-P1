7a690b93c81ff79729f00bb94ce72537c6930879
kamaljit1504 <kaurkamaljit963@gmail.com>
2019-10-30 20:30:41 -0400

    Initial Commit

---
+++ c0772733_MidTerm_MAD3115F2019-P1Tests/c0772733_MidTerm_MAD3115F2019_P1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  c0772733_MidTerm_MAD3115F2019_P1Tests.swift
+//  c0772733_MidTerm_MAD3115F2019-P1Tests
+//
+//  Created by MacStudent on 2019-10-30.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import XCTest
+@testable import c0772733_MidTerm_MAD3115F2019_P1
+
+class c0772733_MidTerm_MAD3115F2019_P1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
